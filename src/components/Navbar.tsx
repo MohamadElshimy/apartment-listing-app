9a1bfe1b9830ee38ed11cf64d0e46bb4aa7569cd
elshimy <mohamad.elshimy.kites@gmail.com>
2025-07-27 02:26:53 +0300

    Enhance Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
             </div>
           </Link>
           
-          <div className="hidden md:flex items-center space-x-8">
+          {/* <div className="hidden md:flex items-center space-x-8">
             <Link href="/">
               <span className="text-gray-700 hover:text-blue-600 transition-colors font-medium cursor-pointer">
                 Browse Properties
@@ -35,7 +35,7 @@ export default function Navbar() {
                 Contact
               </span>
             </Link>
-          </div>
+          </div> */}
 
           <div className="flex items-center space-x-4">
             <Link href="/add-apartment">
